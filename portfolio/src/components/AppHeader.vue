5aa6e96285a88b87e6f11723c4fc0c84247db542
Christopher State <statec@icloud.com>
2019-02-27 14:12:08 +0100

    Card scroll overflow:fix

--- portfolio/src/components/AppHeader.vue
+++ portfolio/src/components/AppHeader.vue
@@ -1,11 +1,30 @@
 <template>
-  <header class="AppHeader" v-bind:class="{ 'is-removed' : transitioning }">
+  <header v-on:click.stop class="AppHeader" v-bind:class="{ 'is-removed' : transitioning }">
     <!-- <div :class="['container', { 'change': toggled }]" @click="myFunction">
       <div class="bar1"></div>
       <div class="bar2"></div>
       <div class="bar3"></div>
     </div>-->
-    <h1 class="AppHeader-title">{{ title }}</h1>
+    <div class="sticky-wrapper">
+      <nav class="subnav">
+        <div class="row">
+          <ul>
+            <li>
+              <a href="#overview" class="anchor_link">Overview</a>
+            </li>
+            <li>
+              <a href="#field-research" class="anchor_link active">Field Research</a>
+            </li>
+            <li>
+              <a href="#engagement-strategy" class="anchor_link">Prototyping</a>
+            </li>
+            <li>
+              <a href="#visual-program" class="anchor_link">Visual Design</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   </header>
 </template>
 
@@ -71,5 +90,28 @@ a {
 .change .bar3 {
   -webkit-transform: rotate(45deg) translate(-8px, -8px);
   transform: rotate(45deg) translate(-8px, -8px);
+}
+.subnav {
+  border-bottom: 1px solid #dbdbdb;
+  padding: 3rem 0;
+  background-color: #fff;
+}
+.subnav.stuck {
+  animation: b 0.2s ease forwards;
+  background-color: #fff;
+  border-bottom: none;
+  left: 0;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1000;
+  padding-top: 0;
+  padding-bottom: 2rem;
+}
+
+.row {
+  max-width: 1300px;
+  margin-right: auto;
+  margin-left: auto;
 }
 </style>
--- portfolio/src/components/AppList.vue
+++ portfolio/src/components/AppList.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="hello">
+    <!-- <h1>Yolo</h1>
+    <h2>I hope you are good!</h2>My name is Christopher State. I am currently studying Interactive Media Technology at KTH. My passion is to create solutions with the user in mind. I have interests in fields from development, design to communcations and marketing. Recently I am very fond of conversational interfaces and how one can use empathy. In my freetime I like to meditate, bike and read.
+    <br>
+    <br>LinkedIn, Twitter, E-Mail, Pidgeon (Postal)-->
     <section>
       <a
         href="#"
@@ -27,15 +31,22 @@
               <!-- {{ card.description }}-->
             </p>
             <div>
-              <span v-html="card.publish_date"></span>
-              <span></span>
+              <span v-html="card.title"></span>
+              <br>
+              <span v-html="card.subtitle"></span>
             </div>
           </div>
         </div>
         <div v-if="card.description" class="Card-body-description">
           <!-- <div style="overflow:scroll" v-html="card.description"></div>-->
-          <div class="container-fluid content-holder">
-            <div class="inner-content" data-position="20vh" data-position-expanded="40vh">
+          <div v-on:click.stop class="container-fluid content-holder">
+            <div
+              class="inner-content"
+              id="content-scroll"
+              data-position="20vh"
+              data-position-expanded="40vh"
+            >
+              <AppHeader v-if="transitionIndex !== null && transitioning"/>
               <div class="info-holder">
                 <div class="like-wrapper btn-liked">
                   <i class="fa fa-heart fa-lg"></i>
@@ -45,7 +56,8 @@
                   <i class="fa fa-bookmark-o fa-lg"></i>
                 </div>
               </div>
-              <h2 class="title" v-html="card.title"></h2>
+              <!--<h2 class="title" v-html="card.title"></h2>
+              <h2 class="title" v-html="card.subtitle"></h2>-->
               <div v-for="item in card.description" :key="item">
                 <div v-html="item"></div>
               </div>
@@ -58,10 +70,15 @@
 </template>
 
 <script>
+import AppHeader from "./AppHeader.vue";
+
 export default {
   name: "AppList",
   props: {
     msg: String
+  },
+  components: {
+    AppHeader
   },
   methods: {
     transitionToFull: function(event, index) {
@@ -113,7 +130,8 @@ export default {
       items: [
         {
           message: "Studs - Studieresan",
-          publish_date: "Sketch, Illustrator, InDesign | Summer 2018",
+          title: "Art Director",
+          subtitle: "Sketch, InDesign | Summer 2018",
           description: [
             "<img width='100%' src='http://cstate.se/assets/studs_screen4.jpg'>",
             "<img width='100%' src='http://cstate.se/assets/studs_screen2.jpg'>",
@@ -128,10 +146,11 @@ export default {
           }
         },
         {
-          message: "Design Thinking - Key-experience",
-          publish_date: "Sketch | February 2019",
+          message: "Key-experience",
+          title: "ME2818 Design Thinking",
+          subtitle: "Sketch | February 2019",
           description: [
-            "<img max-width='300px' src='http://cstate.se/assets/Cardinal_screen1.png'>",
+            "<img src='http://cstate.se/assets/Cardinal_screen1.png'>",
             "<img src='http://cstate.se/assets/Cardinal_screen2.png'>",
             "<img src='http://cstate.se/assets/Cardinal_screen3.png'>",
             "<img src='http://cstate.se/assets/Cardinal_screen4.png'>"
@@ -145,7 +164,8 @@ export default {
         },
         {
           message: "THS Website",
-          publish_date: "AngularJS | 2017/2019",
+          title: "UX Developer",
+          subtitle: "AngularJS | 2017/2019",
           description: [
             "<a href='https://kths.se/'>Link to website</a>",
             "<img width='100%' src='http://cstate.se/assets/ths_screen6.png'>"
@@ -159,7 +179,8 @@ export default {
         },
         {
           message: "CreativeStudio",
-          publish_date: "HTML5/PHP/Javascript | Summer 2018",
+          title: "UX Developer",
+          subtitle: "HTML5/PHP/Javascript | Summer 2018",
           description: [
             "<img width='100%' src='http://cstate.se/assets/creativestudio_screen6.jpg'>",
             "<img width='100%' src='http://cstate.se/assets/creativestudio_screen4.jpg'>",
@@ -279,7 +300,6 @@ button:focus {
   -webkit-overflow-scrolling: touch;
   bottom: 0;
   left: 0;
-  overflow: auto;
   right: 0;
   top: 0;
   z-index: 9999;
@@ -331,6 +351,15 @@ a.Card.is-active .Card-body-description {
 .Card-body-title {
   text-align: center;
 }
+@media screen and (min-width: 768px) {
+  .Card-body-title {
+    text-align: center;
+    font-size: 1.3em;
+  }
+  a.Card.is-active .Card-body-description {
+    padding: 50px;
+  }
+}
 .Card-body-description {
   opacity: 0;
   height: 0;
@@ -345,21 +374,26 @@ a.Card.is-active .Card-body-description {
 }
 
 /** **/
+.Card.is-active .Card-body {
+  display: none;
+}
 .content-holder {
   margin: 0 auto;
   padding: 0;
   z-index: 1;
-  height: 100%;
+  height: 100vh;
+  overflow: scroll;
   background-color: transparent;
   position: relative;
 }
 
 .inner-content {
   margin: 0 auto;
-  max-width: 90vw;
-  top: 10vh;
+  max-width: 100vw;
+  top: 0;
   padding: 50px;
   position: relative;
+  overflow: scroll;
   background-color: #fff;
 }
 body.mobile-scroll.no-scroll
@@ -371,9 +405,25 @@ body.mobile-scroll.no-scroll
   div.inner-content {
   padding: 50px 20px;
 }
+body.mobile-scroll.no-scroll
+  div#app
+  section
+  a.Card.is-active
+  div.Card-body-description
+  div.container-fluid.content-holder
+  div.inner-content
+  div
+  div
+  img {
+  width: 300px;
+  max-width: 100%;
+}
 .inner-content {
   padding-bottom: env(safe-area-inset-bottom); /* for iPhone X */
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
+}
+.inner-content div div img {
+  max-width: 300px;
 }
 </style>
